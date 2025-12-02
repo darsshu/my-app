154f7653bf09931c6d23907de293d8fc5adfebfc
darsshu <chhatroladarshan9@gmail.com>
2025-12-02 08:54:02 +0530

    base crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Crud from './component/Crud';
+import BestCrud from './component/BestCrud';
+import NewCrud from './component/NewCrud';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation */}
+        <nav className=' bg-black text-white'>
+          <ul className='flex justify-between'>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/crud">Crud</Link>
+            </li>
+            <li>
+              <Link to="/crud2">Crud2</Link>
+            </li>
+            <li>
+              <Link to="/newcrud">newcrud</Link>
+            </li>
+            <li>
+              <Link to="/todayscrud">newcrud</Link>
+            </li>
+
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/crud" element={<About />} />
+          <Route path="/crud2" element={<Crud />} />
+          <Route path="/newcrud" element={<BestCrud />} />
+          <Route path="/todayscrud" element={<NewCrud />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
